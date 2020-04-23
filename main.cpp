1f80772dc4f475f75c8897445a6d8d2bb3afcdb3
ShuaiYang <54653335+ShuaiYang-J@users.noreply.github.com>
2020-04-23 15:55:50 +0800

    Create main.cpp
    主函数，调用库包括 picture.h 和game.h

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//三国杀小游戏简化版---仅包括:杀、闪、桃
+//编写环境: VS2017+EasyX
+//作者：杨帅    学号：201928002209011
+//友情提示 鼠标控制延迟有点大，没想好怎么优化 不要砸电脑哦！
+
+#include"game.h"
+#include"picture.h"
+
+int main()
+{    
+	game play;
+	play.Star();
+	return 0;
+}
